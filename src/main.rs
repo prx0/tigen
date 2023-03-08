edc1495c512e5a62a4c5c5dca57e48bd30140857
Maximilien DI DIO <mdidio+dev@pm.me>
2023-03-09 00:19:50 +0100

    add ability to write dockerfile into a specify filepath

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use clap::{arg, command, Parser};
-use std::{fmt::Display, str::FromStr};
+use std::{
+    fmt::Display,
+    fs,
+    io::Write,
+    path::{Path, PathBuf},
+    str::FromStr,
+};
 use tera::{Context, Tera};
 
 const DOCKERFILE: &str = "Dockerfile";
@@ -9,10 +15,15 @@ const TEMPLATE_DIR: &str = "templates/*";
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    #[arg(short, long)]
+    #[arg(short, long, help = "Linux distribution")]
     distro: String,
 
-    #[arg(short, long, default_value = "latest")]
+    #[arg(
+        short,
+        long,
+        default_value = "latest",
+        help = "Distribution release version"
+    )]
     release: String,
 }
 
@@ -152,6 +163,7 @@ impl std::error::Error for DecodingError {}
 enum Error {
     Decoding(DecodingError),
     Templating(tera::Error),
+    IO(std::io::Error),
 }
 
 impl From<DecodingError> for Error {
@@ -163,6 +175,12 @@ impl From<DecodingError> for Error {
 impl From<tera::Error> for Error {
     fn from(err: tera::Error) -> Self {
         Self::Templating(err)
+    }
+}
+
+impl From<std::io::Error> for Error {
+    fn from(err: std::io::Error) -> Self {
+        Self::IO(err)
     }
 }
 
@@ -201,18 +219,50 @@ where
     format!("{} && {} && {}", update, upgrade, install)
 }
 
+#[derive(Debug)]
+struct ImageMetadata<'a> {
+    distro: &'a str,
+    release: &'a str,
+    dockerfile: String,
+}
+
+impl<'a> ImageMetadata<'a> {
+    fn try_new(distro: &'a str, release: &'a str) -> Result<Self, Error> {
+        let dist = Distro::from_str(distro)?;
+        let tera = Tera::new(TEMPLATE_DIR)?;
+
+        let mut context = Context::new();
+        context.insert("name", distro);
+        context.insert("version", release);
+        context.insert("run_layer", &dist.run_layer());
+
+        let dockerfile = tera.render(DOCKERFILE, &context)?;
+        Ok(Self {
+            distro,
+            release,
+            dockerfile,
+        })
+    }
+
+    fn dir(&self) -> PathBuf {
+        PathBuf::from(format!(
+            "images/{}/{}/Dockerfile",
+            self.distro, self.release
+        ))
+    }
+}
+
+fn genenerate_image_path(image: &ImageMetadata<'_>) -> Result<(), Error> {
+    let path = image.dir();
+    fs::create_dir_all(&path)?;
+    let mut file = fs::File::create(path.join(Path::new("Dockerfile")))?;
+    file.write_all(image.dockerfile.as_bytes())?;
+    Ok(())
+}
+
 fn main() -> Result<(), Error> {
     let args = Args::parse();
-    let distro = Distro::from_str(&args.distro)?;
-
-    let tera = Tera::new(TEMPLATE_DIR)?;
-
-    let mut context = Context::new();
-    context.insert("name", &args.distro);
-    context.insert("version", &args.release);
-    context.insert("run_layer", &distro.run_layer());
-
-    let rendered = tera.render(DOCKERFILE, &context)?;
-    println!("{}", rendered);
+    let image = ImageMetadata::try_new(&args.distro, &args.release)?;
+    genenerate_image_path(&image)?;
     Ok(())
 }
